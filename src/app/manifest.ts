26fb36510143e20071543217ae90146156afebdd
kumarlalait79 <luckyarya0101@gmail.com>
2026-07-04 12:49:45 +0530

    fixed size

--- src/app/manifest.ts
+++ src/app/manifest.ts
@@ -11,13 +11,8 @@ export default function manifest(): MetadataRoute.Manifest {
     theme_color: "#15803d", // your green theme
     icons: [
       {
-        src: "/logo-192.png",
-        sizes: "192x192",
-        type: "image/png",
-      },
-      {
-        src: "/logo-512.png",
-        sizes: "512x512",
+        src: "/logo.png",
+        sizes: "483x517",
         type: "image/png",
       },
     ],
